cf6670add29e1da1cf1cedea4fbbd87a7aa0b539
Diogo Cruz <diogo.abc.cruz@gmail.com>
2023-09-05 11:16:53 +0000

    Added old reward to troubleshoot.

--- rl4lms/envs/text_generation/sentiment_reward.py
+++ rl4lms/envs/text_generation/sentiment_reward.py
@@ -91,6 +91,63 @@ class SentimentRewardIMDBFunction(RewardFunction):
                 meta_info['label']
             )
         else:
+            return 0      # this isn't really ideal
+
+class SentimentRewardFunction(RewardFunction):
+    ''' A controlled sentiment generation reward function 
+    Uses a pretrained model to classify the sentiment of the generated text.
+    Returns the cross entropy of the classifier prediction with the desired label.
+    '''
+    MODEL_NAME = f"cardiffnlp/twitter-roberta-base-sentiment-latest"
+    model = None
+    tokenizer = None
+    config = None
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    @staticmethod
+    def preprocess(text):
+        new_text = []
+        for t in text.split(" "):
+            t = '@user' if t.startswith('@') and len(t) > 1 else t
+            t = 'http' if t.startswith('http') else t
+            new_text.append(t)
+        return " ".join(new_text)
+
+    @classmethod
+    def _compute_sentiment(cls, text) -> torch.Tensor:
+        if cls.model is None: 
+            cls.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+            cls.model = AutoModelForSequenceClassification.from_pretrained(
+                cls.MODEL_NAME
+            ).to(cls.device)
+            cls.tokenizer = AutoTokenizer.from_pretrained(cls.MODEL_NAME)
+            cls.config = AutoConfig.from_pretrained(cls.MODEL_NAME)
+
+        text = SentimentRewardFunction.preprocess(text)
+        encoded_input = cls.tokenizer(text, return_tensors='pt').to(cls.device)
+        output = cls.model(**encoded_input)
+        scores = output[0][0].detach()
+        return torch.softmax(scores, dim=0)
+
+    @classmethod
+    def compute_reward(cls, text, label) -> float:
+        sentiment_score = cls._compute_sentiment(text)
+        reward = torch.log(sentiment_score[label])
+        return reward.item()
+    
+    def __call__(self, prev_observation: Observation,
+                 action: int,
+                 current_observation: Observation,
+                 done: bool,
+                 meta_info: Dict[str, Any] = None) -> float:
+        if done:   
+            return self.compute_reward(
+                current_observation.context_text,
+                meta_info['label']
+            )
+        else:
             return 0      # this isn't really ideal 
 
         
